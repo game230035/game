ea4db52004edcde6c78eb7fcbce12533ef0d6920
Combo_macmini <>
2020-10-26 07:03:35 +0900

    blog

--- blog/shell/gamepush.sh
+++ blog/shell/gamepush.sh
@@ -5,5 +5,6 @@ hugo
 cd /Users/comboengineering/work/blog/game
 rm -rf docs
 cp -rp /Users/comboengineering/work/blog/game/blog/public ./docs
-git add -a
+git add --all
 git commit -m "blog"
+git push
