baa8ca9f0579aba1ca1810d5e86ec231ffc1ee41
Combo_macmini <>
2020-11-11 16:12:04 +0900

    blog

--- blog/shell/create.sh
+++ blog/shell/create.sh
@@ -4,8 +4,9 @@ if [ $# != 1 ]; then
 	echo タイトルを引数にしてください: 
 	exit 1
 else
-	hugo new posts/$1.md
+	cd /Users/comboengineering/work/blog/game/blog
+	hugo new posts/$1/index.md
 	echo OK
-	open /Users/comboengineering/work/blog/game/blog/content/posts/$1.md
+	open /Users/comboengineering/work/blog/game/blog/content/posts/$1/index.md
 fi
 
