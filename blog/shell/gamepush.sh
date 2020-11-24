7bbdc797ebecf9bb59d0284c8af240218e38393a
Combo_macmini <>
2020-11-24 22:44:51 +0900

    blog

--- blog/shell/gamepush.sh
+++ blog/shell/gamepush.sh
@@ -8,6 +8,8 @@ hugo
 cd /Users/comboengineering/work/blog/game
 rm -rf docs
 cp -rp /Users/comboengineering/work/blog/game/blog/public ./docs
+# ややこしいから消す
+rm -rf /Users/comboengineering/work/blog/game/blog/public
 git add --all
 git commit -m "blog"
 git push
