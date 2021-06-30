ff8ec4e7cf5efc388ff5ab4258af3a3bc65831e0
Combo_macmini <>
2021-06-30 23:54:51 +0900

    blog

--- blog/shell/create.sh
+++ blog/shell/create.sh
@@ -5,7 +5,7 @@ if [ $# != 1 ]; then
 	exit 1
 else
 	cd /Users/comboengineering/work/blog/game/blog
-	hugo new posts/$1/index.md
+	hugo new posts/$1/index.md -k $2
 	echo OK
 	open /Users/comboengineering/work/vscode_shortcut/gameblog.code-workspace
 	hugo server
